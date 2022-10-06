45b2e9a701d3b28d9e3cc632a75d8f0e606d31ff
MishimaPorte <sublime_porte@mail.ru>
2022-10-06 04:46:06 -0400

    __init__.py added

--- bot.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Dispatcher, Bot
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from configparser import ConfigParser
-
-config = ConfigParser()
-config.read("bot.ini")
-
-bot = Bot(token=config["Bot"]["token"])
-dispatcher = Dispatcher(bot, storage=MemoryStorage())
